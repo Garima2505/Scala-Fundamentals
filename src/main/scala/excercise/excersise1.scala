deb15496d7398acdf33b537022b9f91a4738d5a2
Garima Chaudhary <gc2505@nyu.edu>
2021-08-18 12:20:03 -0400

    update

---
+++ src/main/scala/excercise/excersise1.scala
@@ -0,0 +1,46 @@
+package excercise
+
+import java.security.KeyStore.TrustedCertificateEntry
+
+object excersise1 extends App {
+
+  // Greeting Function (name, age)
+  def greeting(name: String, age: Int): Unit = {
+    print("Hi, my name is " + name + " I'm " + age + " year old")
+  }
+  greeting("sam", 12)
+
+  println(greeting("sam", 5))
+
+  // Factorial Function
+  def factorial(x : Int): Int = {
+
+    if (x == 1) 1 else x * factorial(x-1)
+  }
+
+  println(factorial(4))
+
+  // Fibonacci
+  def fibonacci(x: Int): Int = {
+    if ((x == 1) || (x == 2)) 1
+    else fibonacci(x - 1) + fibonacci(x - 2)
+
+  }
+
+  println(fibonacci(4))
+
+
+  // Check Prime
+  def checkPrime(x: Int): Boolean = {
+
+      def repeat(n: Int): Boolean = {
+        if (n<= 1) true
+        else x%n != 0 && repeat(n-1)
+
+      }
+    repeat(x/2)
+  }
+
+  print(checkPrime(2))
+
+}
--- src/main/scala/lecture/part1_basics/Functions.scala
+++ src/main/scala/lecture/part1_basics/Functions.scala
@@ -6,11 +6,40 @@ object Functions extends App {
   def aFunction(a: String, b: Int): String =
     a +  " " + b
 
+  // Compiler cam figure out the return type so we don't need to specify the return type
 
   def aFunctionBlock(a: String, b: Int): String = { // can also write as a block
     a +  " " + b
   }
 
-  println(aFunction("hello", 4))
+  println(aFunction("hello", 4))  // Calling a function is also an expression
+
+  //parameter less functions can also be call just by name
+  // exmple: aFunction() == aFunction
+
+  // Recursive function -> When you need loop use RECURSIVE FUNCTION
+  def aRepeatedFunction(n: Int, aString: String): String = {
+    if (n == 1) aString
+    else aString + ' ' + aRepeatedFunction(n-1, aString)
+  }
+
+  // Though compiler can figure out the return type of a function, it cannot figure out the return type of a
+  // recursive function. So it's a best practice to always specify the return type.
+  println(aRepeatedFunction(3, "hello"))
+
+  // Unit as return type -> defining a function that uses side effect
+  def aFucntionWithSideEffect(aString: String): Unit = println(aString)
+
+  aFucntionWithSideEffect("Side Effect")
+
+  // code block also allow auxiliary function
+  def aBigFunction(n: Int): Int = {
+
+    def aSmallFunction(a: Int, b: Int): Int = a+b
+
+    aSmallFunction(n, n-1)
+  }
+
+
 
 }
