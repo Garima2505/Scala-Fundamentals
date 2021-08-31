3d93a012ca5f8abe8aeef66b27817fe936da8f06
Garima Chaudhary <gc2505@nyu.edu>
2021-08-31 16:52:27 -0400

    update

--- src/main/scala/lecture/part1_basics/CallingFunction.scala
+++ src/main/scala/lecture/part1_basics/CallingFunction.scala
@@ -2,6 +2,26 @@ package lecture.part1_basics
 
 object CallingFunction extends App{
 
+  def callByValue(x: Long): Unit = {
+    println("by value: " + x)
+    println("by value: " + x)
+  }
 
+  def callByName(x: => Long): Unit = { // calling by name as value is evaluated every time
+    println("by value: " + x)
+    println("by value: " + x)
+  }
+
+  callByValue(System.nanoTime())
+  // Return identical value for both print statements
+
+  callByName(System.nanoTime())
+  // return different values for both the print statements
+  // because value of x is evaluated twice
+  // useful in Lazy streams
+
+  // Call by Value: value is computed before call and same value used everywhere
+
+  // Call by Name: expression is passed literally and expression is evaluated at every use within
 
 }
